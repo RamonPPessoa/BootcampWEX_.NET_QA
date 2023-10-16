e07fde9bee186959f07caf71c0aa2335d7e1e82e
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-10-16 14:45:05 -0400

    Bootcamp WEX - Desenvolvimento .NET e QA

---
+++ DesenvolvimentoOrientadoATestes/TestesUnitáriosC#/Calculadora/Program.cs
@@ -0,0 +1,8 @@
+﻿using Calculadora.Services;
+
+CalculadoraImplementacao c = new CalculadoraImplementacao();
+
+int num1 =20;
+int num2 = 30;
+
+System.Console.WriteLine($"{num1} + {num2} = {c.Somar(num1,num2)}");
---
+++ DesenvolvimentoOrientadoATestes/TestesUnitáriosC#/Calculadora/Services/ValidacoesString.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora.Services
+{
+    public class ValidacoesString
+    {
+        public int ContaCaracteres (string texto)
+        { 
+
+            int num = texto.Length;
+
+            return num;
+         }
+    }
+}
