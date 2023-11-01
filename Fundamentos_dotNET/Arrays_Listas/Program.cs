4d4d172f7c594dd3e56c1d16e23df834a475c0d6
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-11-01 18:43:27 -0400

    Percorrendo um array com foreach

--- Fundamentos_dotNET/Arrays_Listas/Program.cs
+++ Fundamentos_dotNET/Arrays_Listas/Program.cs
@@ -5,7 +5,19 @@ arrayInteiros[0] = 72;
 arrayInteiros[1] = 64;
 arrayInteiros[2] = 50;
 
+Console.WriteLine("Percorrendo com o FOR");
+
 for (int i = 0; i < arrayInteiros.Length; i++)
 {
     Console.WriteLine($"Posição Nº{i} - {arrayInteiros[i]}");
+}
+
+
+Console.WriteLine("Percorrendo com o FOREACH");
+int contadorForeach = 0;
+foreach (int item in arrayInteiros)
+{
+    Console.WriteLine($"Posição Nº {contadorForeach}- {item}");
+    contadorForeach++;
+
 }
