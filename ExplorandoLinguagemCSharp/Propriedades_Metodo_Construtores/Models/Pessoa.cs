484e674487be286ef688ce446e1bb2b294635f6a
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-11-21 15:15:11 -0400

    Criando a classe e suas propriedades

---
+++ ExplorandoLinguagemCSharp/Propriedades_Metodo_Construtores/Models/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propriedades_Metodo_Construtores.Models
+{
+     public class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+    }
+}
