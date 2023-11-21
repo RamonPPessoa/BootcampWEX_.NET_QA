3a2fc475c8d041d41721714621267ede1a2c268b
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-11-21 18:42:44 -0400

    Utilizando validações no get e set

--- ExplorandoLinguagemCSharp/Propriedades_Metodo_Construtores/Models/Pessoa.cs
+++ ExplorandoLinguagemCSharp/Propriedades_Metodo_Construtores/Models/Pessoa.cs
@@ -7,8 +7,29 @@ using System.Threading.Tasks;
 namespace Propriedades_Metodo_Construtores.Models
 {
      public class Pessoa
+
+       
     {
-        public string Nome { get; set; }
+        private string _nome;
+        public string Nome 
+        {
+            get
+            {
+                return _nome.ToUpper();
+            }
+            
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException(" O nome não pode ser vazio");
+
+                }
+                _nome = value;
+
+            }
+        
+        }
 
         public int Idade { get; set; }
 
