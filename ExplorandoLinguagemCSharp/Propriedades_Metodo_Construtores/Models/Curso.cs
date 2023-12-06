94037657e6cea78099dedf3c732057b01022d9bd
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-12-06 19:10:47 -0400

    Implementação da classe Curso

---
+++ ExplorandoLinguagemCSharp/Propriedades_Metodo_Construtores/Models/Curso.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propriedades_Metodo_Construtores.Models
+{
+    internal class Curso
+    {
+    }
+}
