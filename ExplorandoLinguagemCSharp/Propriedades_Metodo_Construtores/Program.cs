51a67c77764ea266f2105ca6b84411ecd52d6518
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-12-20 10:23:49 -0400

    Implementando métodos com retorno,métodos de remoção, e métodos de listar

--- ExplorandoLinguagemCSharp/Propriedades_Metodo_Construtores/Program.cs
+++ ExplorandoLinguagemCSharp/Propriedades_Metodo_Construtores/Program.cs
@@ -1,8 +1,34 @@
 ﻿using Propriedades_Metodo_Construtores.Models;
 
+
+
 Pessoa p1 = new Pessoa();
 
 p1.Nome = "Fernanda";
 p1.Sobrenome = "Silva";
-p1.Idade = 30;
-p1.Apresentar();
+
+
+Pessoa p2 = new Pessoa();
+
+p2.Nome = "Camila";
+p2.Sobrenome = "Santos";
+
+Curso cursoDeIdioma = new Curso();
+cursoDeIdioma.Nome = "Ingês";
+cursoDeIdioma.Alunos = new List<Pessoa>();
+
+cursoDeIdioma.AdicionarAluno(p1);
+cursoDeIdioma.AdicionarAluno(p2);
+cursoDeIdioma.ListarAlunos();
+
+
+
+
+
+
+//Pessoa p1 = new Pessoa();
+
+//p1.Nome = "Fernanda";
+//p1.Sobrenome = "Silva";
+//p1.Idade = 30;
+//p1.Apresentar();
