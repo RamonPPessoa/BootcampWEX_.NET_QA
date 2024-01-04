bf98e21d5b0992d1ac74e6ae32b9ff1708bf5ff5
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2024-01-04 16:50:04 -0400

    Abstração e encapsulamento

---
+++ POO/AbstracaoEncapsulamento/Models/ContaCorrente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstracaoEncapsulamento.Models
+{
+    public class ContaCorrente
+    {
+        public ContaCorrente(int numeroConta,decimal saldoInicial )
+        {
+            NumeroConta = numeroConta;
+            saldo = saldoInicial;
+        }
+
+        public int NumeroConta { get; set; }
+
+        private decimal saldo { get; set; }
+
+        public void Sacar(decimal valor)
+        {
+            if (saldo >= valor)
+            {
+                saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente");
+            }
+
+                                        
+        }
+
+        public void ExibirSaldo()
+        {
+            Console.WriteLine("Seu saldo disponivel é:" + saldo);
+        }
+    }
+}
--- POO/AbstracaoEncapsulamento/Program.cs
+++ POO/AbstracaoEncapsulamento/Program.cs
@@ -1,9 +1,14 @@
 ﻿using AbstracaoEncapsulamento.Models;
 
+ContaCorrente conta1 = new ContaCorrente(123,1000);
+conta1.ExibirSaldo();
+conta1.Sacar(500);
+conta1.ExibirSaldo();
 
-Pessoa pessoa1 = new Pessoa();
 
-pessoa1.Nome = "Fernanda";
-pessoa1.Idade = 18;
+//Pessoa pessoa1 = new Pessoa();
 
-pessoa1.Apresentar();
+//pessoa1.Nome = "Fernanda";
+//pessoa1.Idade = 18;
+
+//pessoa1.Apresentar();
