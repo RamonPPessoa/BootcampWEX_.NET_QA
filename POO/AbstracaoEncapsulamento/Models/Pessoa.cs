0e0dda2f153facccf7013a60289a0d2b04277393
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2024-01-04 10:58:20 -0400

    Programação Orientado a Objetos

---
+++ POO/AbstracaoEncapsulamento/Models/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstracaoEncapsulamento.Models
+{
+    public class Pessoa
+    {
+
+        public string Nome { get; set; }
+
+        public int Idade{ get; set;}
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Ola seu nome é: {Nome} e usa idade é {Idade}");
+        }
+    }
+}
---
+++ POO/AbstracaoEncapsulamento/Program.cs
@@ -0,0 +1,9 @@
+﻿using AbstracaoEncapsulamento.Models;
+
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Fernanda";
+pessoa1.Idade = 18;
+
+pessoa1.Apresentar();
