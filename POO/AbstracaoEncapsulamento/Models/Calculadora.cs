e524c122f7062a5e819804bf0cdfc38812bc6549
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2024-01-17 17:41:59 -0400

    Trabalhando com interfaces e método padrão na interface

---
+++ POO/AbstracaoEncapsulamento/Models/Calculadora.cs
@@ -0,0 +1,29 @@
+﻿using AbstracaoEncapsulamento.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstracaoEncapsulamento.Models
+{
+    public class Calculadora : ICalculadora
+    {
+        public int Dividir(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Somar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+      
+    }
+}
